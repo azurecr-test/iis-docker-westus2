ae38a1aee98f718add5217e92db69ebcb6ecd348
Bin Du <2686301+northtyphoon@users.noreply.github.com>
2021-03-10 09:37:16 -0800

    add retry

--- windowsservercore-ltsc2019new/check-baseimage.ps1
+++ windowsservercore-ltsc2019new/check-baseimage.ps1
@@ -9,7 +9,27 @@ $currUBR=$current.UBR
 
 # fetch the maximum version number from MCR by filtering and sorting the JSON result
 $prefix="$($current.CurrentMajorVersionNumber).$($current.CurrentMinorVersionNumber).$($current.CurrentBuildNumber)."
-$json=$(Invoke-WebRequest -UseBasicParsing https://mcr.microsoft.com/v2/windows/servercore/tags/list | ConvertFrom-Json)
+
+try
+{
+    $json=$(Invoke-WebRequest -UseBasicParsing https://mcr.microsoft.com/v2/windows/servercore/tags/list | ConvertFrom-Json)
+}
+catch [System.Net.WebException]
+{
+  $ex =  $PSItem.Exception
+  Write-Host "Error is " $ex.Message
+  
+  foreach($key in $ex.Response.Headers.Keys)
+  {
+     Write-Host "Response Header " $key "::" $ex.Response.Headers[$key]     
+  }
+
+  Write-Host "Waiting 3 seconds and try again"
+  Start-Sleep -Seconds 3
+
+  $json=$(Invoke-WebRequest -UseBasicParsing https://mcr.microsoft.com/v2/windows/servercore/tags/list | ConvertFrom-Json)
+}
+
 $hubUBR=($json.tags | Where-Object -FilterScript { $_.StartsWith($prefix) -and $_ -Match "^\d+\.\d+\.\d+\.\d+$" } |%{[System.Version]$_}|sort)[-1].Revision
 
 Write-Output "Base image Update Build Revision $currUBR, Hub Update Build Revision $hubUBR"
